0db0953c33136dfba55763345ff3d75924bd8448
LAPTOP-QP2IV66C\Olek <abuszek@student.agh.edu.pl>
2024-06-16 19:36:58 +0200

    music

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import pygame
 from MainGame import *
 
+def play_music():
+    pygame.mixer.init()
+    pygame.mixer.music.load("./resources/muzyka.mp3") 
+    pygame.mixer.music.play(-1)
+
+def stop_music():
+    pygame.mixer.music.stop()
+    pygame.mixer.quit()
+
 if __name__ == "__main__":
-    game = MainGame()
-    game.play()
+    play_music()
+    
+    try:
+        game = MainGame()
+        game.play()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        stop_music()
